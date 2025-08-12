66bdb732b87e76362c2cee20466f8e88e8a261dd
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-08-12 13:52:21 +0530

    Fix indentation for gunicorn; accept floats in predict

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ model2= pickle.load(open('model.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    int_features=[int(x) for x in request.form.values()] #it stores like [1,2,3,4]
+    # Accept numeric fields as float to handle decimals from the form
+    feature_values = [float(x) for x in request.form.values()]
+    final_features = np.array(feature_values).reshape(1, -1)
+    prediction = model2.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template(
+        'index.html',
+        prediction_text='Number of weekly riders is {}'.format(math.floor(output))
+    )
 
-    final_features=np.array(int_features).reshape(1,-1) #converts like [1,2,3,4] to [[1,2,3,4]]
-
-    prediction=model2.predict(final_features)
-    output = round(prediction[0],2)
-
-    return render_template('index.html',prediction_text='Number of weekly riders is {}'.format(math.floor(output)))
-
-if __name__=='__main__':s
+if __name__ == '__main__':
     app.run(debug=True)
